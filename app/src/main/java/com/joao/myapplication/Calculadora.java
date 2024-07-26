ba0e232576e9935a76b2f801b71d2c6f7c9f8284
joao <joaovitor502013@gmail.com>
2024-07-26 09:07:18 -0300

    atualização classe calculadora

--- app/src/main/java/com/joao/myapplication/Calculadora.java
+++ app/src/main/java/com/joao/myapplication/Calculadora.java
@@ -1,24 +1,23 @@
 package com.joao.myapplication;
 
+import android.net.wifi.aware.PublishConfig;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.function.BiFunction;
+
 public class Calculadora {
-
-    private static final int MODO_EDITANDO = 0;
-
-    private static final int MODO_EXIBINDO = 1;
-
+    public static final int MODO_EDITANDO = 0;
+    public static final int MODO_EXIBINDO = 1;
+    public static final int MODO_ERRO = 2;
     private double numero;
-
     private Deque<Double> operandos;
+    private int modo = MODO_EXIBINDO;
 
-    private int modo = MODO_EXIBINDO;,
-
-    public Calculadora(){
+    public Calculadora() {
         numero = 0;
         operandos = new LinkedList<>();
-    }
-
-    public double getNumero() {
-        return numero;
     }
 
     public void setNumero(double numero) {
@@ -26,20 +25,53 @@ public class Calculadora {
         modo = MODO_EDITANDO;
     }
 
-    public void enter(){
-        if(modo == MODO_EDITANDO){
+    public double getNumero() {
+        return numero;
+    }
+
+    public int getModo() {
+        return modo;
+    }
+
+    public void enter() {
+        if (modo == MODO_ERRO) {
+            modo = MODO_EXIBINDO;
+        }
+        if (modo == MODO_EDITANDO) {
             operandos.push(numero);
-            modo=MODO_EXIBINDO;
+            modo = MODO_EXIBINDO;
         }
     }
 
-    public void soma(){
-        if(modo == MODO_EDITANDO){
+    protected void executarOperacao(BiFunction<Double, Double, Double> operacao) {
+        if (modo == MODO_EDITANDO || modo == MODO_ERRO) {
             enter();
         }
-        double op1 = operandos.pop();
-        double op2 = operandos.pop();
-        numero = op1 + op2;
+        double op1 = Optional.ofNullable(operandos.pollFirst()).orElse(0.0);
+        double op2 = Optional.ofNullable(operandos.pollFirst()).orElse(0.0);
+        numero = operacao.apply(op1, op2);
         operandos.push(numero);
     }
-}
+    public void soma() {
+        executarOperacao((op1, op2) -> op1 + op2);
+    }
+
+    public void subtracao() {
+        executarOperacao((op1, op2) -> op1 - op2);
+    }
+
+    public void multiplicacao() {
+        executarOperacao((op1, op2) -> op1 * op2);
+    }
+
+    public void divisao() {
+        if (modo == MODO_EDITANDO) {
+            enter();
+        }
+        double denominador = Optional.ofNullable(operandos.peek()).orElse(0.0);
+        if (denominador == 0) {
+            modo = MODO_ERRO;
+            return;
+        }
+        executarOperacao((op1, op2) -> op2 / op1);
+    }
